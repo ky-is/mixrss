f676369a972a14fa71bdb11f7867c513d7f99d54
Kyle Coburn <kylecoburn@gmail.com>
2018-07-31 23:51:47 -0700

    ➕ vue-youtube

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
 
 import App from '@/App'
 
 import store from '@/store'
 
 Vue.config.productionTip = false
+
+Vue.use(VueYoutube)
 
 new Vue({
 	render: h => h(App),
