ad601f7a7edd0dba17d5a9f8b3152679e27b4cb1
Kyle Coburn <kylecoburn@gmail.com>
2018-08-02 21:11:50 -0700

    ✨ Sidebar items

---
+++ src/components/Sidebar/Item.vue
@@ -0,0 +1,55 @@
+<template>
+<li @click="onItem" class="feed-item hover-outer">
+	<div class="item-icon" :style="{ 'background-image': `url(${item.icon})` }" />
+	<div>
+		<div class="title">{{ item.title }}</div>
+		<span class="text-faint text-small">{{ item.items ? item.items.length : 0 }} songs ・ {{ item.author.name || 'Unknown' }}</span>
+	</div>
+</li>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import storage from '@/helpers/storage'
+
+export default Vue.extend({
+	props: {
+		url: String,
+	},
+
+	computed: {
+		item (): JSONFeedItem {
+			return storage.getJSON(this.url)
+		},
+	},
+
+	methods: {
+		onItem () {
+		},
+	},
+})
+</script>
+
+<style scoped>
+.feed-item {
+	display: flex;
+	cursor: pointer;
+	align-items: center;
+	padding: 6px;
+	transition: background-color 250ms;
+}
+.feed-item:hover {
+	background-color: #fff;
+}
+
+.item-icon {
+	width: 32px;
+	height: 32px;
+	border-radius: 50%;
+	background-size: cover;
+	background-position: center;
+	margin-right: 4px;
+	flex-shrink: 0;
+}
+</style>
--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -1,15 +1,19 @@
 <template>
 <div class="sidebar">
-	<div v-for="feedUrl in feedList" :key="feedUrl">
-		{{ feedUrl }}
-	</div>
+	<SidebarItem v-for="feedUrl in feedList" :url="feedUrl" :key="feedUrl" />
 </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import SidebarItem from '@/components/Sidebar/Item.vue'
+
 export default Vue.extend({
+	components: {
+		SidebarItem,
+	},
+
 	computed: {
 		feedList (): string[] {
 			return this.$store.state.feed.list
@@ -21,7 +25,6 @@ export default Vue.extend({
 <style scoped>
 .sidebar {
 	width: 100%;
-	padding: 8px;
 	box-sizing: border-box;
 	overflow: hidden scroll;
 }
