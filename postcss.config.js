989b7c951125ff11e20c38ebe31b9621ead30c41
Kyle Coburn <kylecoburn@gmail.com>
2018-08-15 15:40:57 -0700

    ⬆️ tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
-const isDev = process.argv.indexOf('serve') !== -1
-
 module.exports = {
 	plugins: [
-		require('postcss-import'),
+		require('postcss-import')(),
+		require('postcss-preset-env')({ stage: 2 }),
 		require('tailwindcss')('./tailwind.config.js'),
-		isDev ? null : require('@fullhuman/postcss-purgecss')({
+		require('@fullhuman/postcss-purgecss')({
 			content: [ './src/**/*.vue', './src/**/*.@(css|less|sass|scss|styl)' ],
 		}),
-		require('autoprefixer'),
+		require('autoprefixer')(),
 	],
 }
