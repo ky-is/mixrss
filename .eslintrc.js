07988175dd6fcc0144b75dcf20b33bae2976dc46
Kyle Coburn <kylecoburn@gmail.com>
2018-08-15 18:23:18 -0700

    ➕ @ky-is/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,12 @@
 module.exports = {
 	root: true,
+
 	extends: [
-		'plugin:vue/recommended',
-		'eslint:recommended',
+		'@ky-is/eslint-config/vue',
 		'@vue/typescript',
 	],
-	env: {
-		browser: true,
-		node: true,
-	},
+
 	parserOptions: {
-		ecmaVersion: 2017,
 		parser: 'typescript-eslint-parser',
-	},
-	rules: {
-		'comma-dangle': [ 'error', 'always-multiline' ],
-		'curly': 'error',
-		'indent': [ 'error', 'tab' ],
-		'no-console': 0,
-		'no-multiple-empty-lines': [ 'error', {
-			max: 1,
-		}],
-		'no-unused-vars': [ 'warn', {
-			varsIgnorePattern: '^_',
-			argsIgnorePattern: '^_',
-		}],
-		'no-var': 'error',
-		'vue/attribute-hyphenation': 'never',
-		'vue/attributes-order': 'never',
-		'vue/html-indent': 0,
-		'vue/max-attributes-per-line': [ 'error', {
-			singleline: 999,
-			multiline: 0,
-		}],
-		'vue/require-default-prop': 0,
 	},
 }
