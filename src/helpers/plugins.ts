dbcdf695ece948a5fe3ecaa095cbf6204de6d258
Kyle Coburn <kylecoburn@gmail.com>
2018-08-29 08:28:00 -0700

    🏷 DirectiveFunction

--- src/helpers/plugins.ts
+++ src/helpers/plugins.ts
@@ -1,9 +1,9 @@
 import { PluginFunction } from 'vue/types/plugin'
+import { DirectiveFunction } from 'vue/types/options'
 
 const install: PluginFunction<any> = (Vue) => {
-	Vue.directive('focus', {
-		inserted: (el: HTMLElement) => el.focus(),
-	})
+	const inserted: DirectiveFunction = (el) => el.focus()
+	Vue.directive('focus', { inserted })
 }
 
 export default install
