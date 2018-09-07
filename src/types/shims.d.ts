06ea2b768389c32711cba637c0e8e0c598fa6a8e
Kyle Coburn <kylecoburn@gmail.com>
2018-09-07 13:28:06 -0700

    ♻️ Consolodate type shims

--- src/types/shims.d.ts
+++ src/types/shims.d.ts
@@ -2,3 +2,15 @@ declare module '*.vue' {
 	import Vue from 'vue'
 	export default Vue
 }
+
+declare module 'fetch-jsonp'
+
+declare module 'soundcloud'
+
+declare module 'vue-youtube'
+
+declare module 'vue/types/vue' {
+	interface Vue {
+		$youtube: any
+	}
+}
